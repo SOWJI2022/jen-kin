528274901c65ab91a668e5283b1cfc083203b8cf
SOWJI2023 <jajjara.sowjanya@gmail.com>
2023-07-07 10:33:49 +0530

    3 files to be commited

---
+++ jen-kin.sh
@@ -0,0 +1,8 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java
+java --version
+sudo yum install jenkins
+
---
+++ statr-jenkin.sh
@@ -0,0 +1,4 @@
+sudo systemctl status jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
